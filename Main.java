5e164c4dc655afad21e9446152ec958353ae125f
pashazeit <zeitlmd@gmail.com>
2016-10-13 11:35:53 +0300

    testtest

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+
+package com.company;
+
+import java.io.*;
+
+public class Main {
+
+
+    public static void main(String[] args)throws Exception {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+       String a = reader.readLine();
+
+        System.out.println(a);
+
+    }
+}
