21813506be19d017970c73ec2b8b14b9110d310f
pashazeit <zeitlmd@gmail.com>
2016-10-17 23:59:11 +0300

    Add files via upload

---
+++ argum.java
@@ -0,0 +1,19 @@
+package com.company;
+
+/**
+ * Created by Zeit on 17.10.2016.
+ */
+public class argum {
+
+
+        public static void main(String[] args) {
+
+            int numberOfArgs = args.length;
+
+            for (int i=0; i<numberOfArgs; i++)
+            {
+                System.out.println("tst " + args[i]);
+            }
+
+        }
+}
