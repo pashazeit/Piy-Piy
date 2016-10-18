bba40299eeed91e4e86ef3c4b7e076d20d762a5f
pashazeit <zeitlmd@gmail.com>
2016-10-18 13:16:16 +0300

    Update massiv.java

--- massiv.java
+++ massiv.java
@@ -1,23 +1,46 @@
 package com.company;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 /**
  * Created by Zeit on 17.10.2016.
  */
 public class massiv {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int min = 1;
-        int max = 100;
-        int[] mass = new int[5];
-        for (int i = 0; i < 5; i++) {
-            mass[i] = ((int) (Math.random() * max));
+   public static void main(String[] args) {
+        int a = 100000;
+        int[] num = new int[100];
+
+        for (int i = 0; i < num.length - 1; i++) {
+            num[i] = (int) ((Math.random() * a));
         }
-        for (int i = 0; i < mass.length; i++) {
-            System.out.println(mass[i]);
+
+        for (int i : num) {
+            System.out.print(i + " ");
         }
+
+
+        boolean sort = false;
+        while (!sort) {
+
+            sort = true;
+
+            for (int i = 0; i < num.length - 1; i++) {
+
+                if (num[i] > num[i + 1]) {
+
+                    int temp = num[i];
+
+                    num[i] = num[i + 1];
+                    num[i + 1] = temp;
+
+                    sort = false;
+                }
+            }
+        }
+        System.out.println();
+        System.out.println("сортированный   массив");
+        for (int i : num) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.println("min: " + num[0] + " maximum: " + num[num.length - 1]);
     }
+
 }
